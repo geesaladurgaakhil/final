decb1b1995f7462cab3a34d05089361b9fd48720
geesaladurgaakhil <durgaakhil0211@gmail.com>
2020-02-18 22:25:53 +0530

    first commit

---
+++ MavenWebApp1/src/main/resources/SampleServlet.java
@@ -0,0 +1,11 @@
+@WebServlet("/home")
+public class SampleServlet extends HttpServlet {
+	 private static final long serialVersionUID = 1L;
+	   @Override
+	   protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+	         throws ServletException, IOException {
+	      resp.setContentType("text/plain");
+	      resp.getWriter().write("Hello World! Maven Web Project Example.");
+	   }
+
+}
